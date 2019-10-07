a3ea0d020a5cc6fc0b26dd0c02d18a683de7e1a4
takiriki <takita.git@gmail.com>
2019-10-07 16:25:48 +0900

    10月7日　ログイン認証追加２

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.app2')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">主催者の仮登録完了のお知らせ</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,9 +14,12 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    仮登録ありがとうございます!
+                    ご登録のメールアドレスに届いたメールから本登録に進んでください。
                 </div>
             </div>
+            <br>
+            <input type="button" value="TOPに戻る" onclick="location.href='/'">
         </div>
     </div>
 </div>
