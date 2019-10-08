84dc7e39e016088fcac03893b746758a5d3756c2
takiriki <takita.git@gmail.com>
2019-10-08 14:00:58 +0900

    10月8日　ログイン認証失敗

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Response;
 
 class RegisterController extends Controller
 {
@@ -49,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -64,9 +65,20 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            //'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    public function pre_check(Request $request){
+        //$this->validator($request->all())->validate();
+        //flash data
+        $request->flashOnly('email');
+
+        $bridge_request = $request->all();
+        // password マスキング
+        $bridge_request['password_mask'] = '******';
+        return view('auth.register_check');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,9 @@ Route::post('/confirm', 'ContactController@confirm')->name('contact.confirm');
 //お問い合わせ完了画面
 Route::get('/finish', 'ContactController@finish')->name('contact.finish');
 
+//主催者登録ー仮登録画面
+Route::post('register/pre_check', 'Auth\RegisterController@pre_check')->name('register.pre_check');
+
 
 //主催者のTOPページ画面
 Route::group(['middleware' => 'auth'], function(){
